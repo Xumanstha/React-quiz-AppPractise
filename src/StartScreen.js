b6aa9fd7ad7d0bb7850565f473af1e569030f161
Xumanstha <shresthaangura@gmail.com>
2026-08-18 23:38:01 +0545

    button style

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -3,7 +3,7 @@ function StartScreen({ numQuestions }) {
     <div className="start">
       <h2>Welcome to the React Quiz!</h2>
       <h3>{numQuestions} questions to test your react mastery</h3>
-      <button>Let's Start</button>
+      <button className="btn btn-ui">Let's Start</button>
     </div>
   );
 }

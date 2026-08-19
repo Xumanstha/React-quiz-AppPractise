2e976ab8155bea1f8f73588f2b4cce7122366223
Xumanstha <shresthaangura@gmail.com>
2026-08-19 13:04:12 +0545

    add components folder and options component

---
+++ src/components/Options.js
@@ -0,0 +1,12 @@
+function Options({ options }) {
+  return (
+    <div className="options">
+      {options.map((option) => (
+        <button key={option} className="btn btn-option">
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
+export default Options;

20efaa78c289a9a4abcddda0e712fb4c99fa212c
Xumanstha <shresthaangura@gmail.com>
2026-08-19 14:15:13 +0545

    add NextButton component and question is changing

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,15 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
 
 const initialState = {
   questions: [],
   status: "loading",
   //"loading" "error" "active" "finished" "ready" status
   index: 0,
+  answer: null,
+  points: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -20,12 +23,24 @@ function reducer(state, action) {
       return { ...state, status: "error" };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
     default:
       throw new Error("Action Unknown");
   }
 }
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState,
   );
@@ -46,7 +61,17 @@ export default function App() {
           {status === "ready" && (
             <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
           )}
-          {status === "active" && <Question question={questions[index]} />}
+          {status === "active" && (
+            <>
+              <Question
+                key={questions[index].id}
+                question={questions[index]}
+                dispatch={dispatch}
+                answer={answer}
+              />
+              <NextButton dispatch={dispatch} answer={answer} />
+            </>
+          )}
         </Main>
       </div>
     </>

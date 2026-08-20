60846942f7eb3fa910eeb3d8984edb12a175ce88
Xumanstha <shresthaangura@gmail.com>
2026-08-20 15:12:36 +0545

    timer fixed and title fix

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ import Footer from "./Footer";
 import NextButton from "./NextButton";
 import Timer from "./Timer";
 
+const SECS_PER_QUEST = 30;
 const initialState = {
   questions: [],
   status: "loading",
@@ -19,7 +20,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: 0,
-  secondsRemaining: 10,
+  secondsRemaining: null,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -28,7 +29,11 @@ function reducer(state, action) {
     case "dataFailed":
       return { ...state, status: "error" };
     case "start":
-      return { ...state, status: "active" };
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUEST,
+      };
     case "newAnswer":
       const question = state.questions.at(state.index);
       return {
@@ -55,7 +60,7 @@ function reducer(state, action) {
         status: "active",
         points: 0,
         answer: null,
-        secondsRemaining: 10,
+        secondsRemaining: state.questions.length * SECS_PER_QUEST,
       };
     // return { ...initialState, questions: state.questions, state: "active" };
     case "tick":
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 
 function Timer({ dispatch, secondsRemaining }) {
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
   useEffect(() => {
     const intervalId = setInterval(() => {
       dispatch({ type: "tick" });
@@ -9,6 +11,12 @@ function Timer({ dispatch, secondsRemaining }) {
     return () => clearInterval(intervalId);
   }, [dispatch]);
 
-  return <div className="timer">{secondsRemaining}</div>;
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins}:{seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
 }
 export default Timer;

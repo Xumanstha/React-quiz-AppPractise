128c10e47c02d0429a90d34b10bddb273459480b
Xumanstha <shresthaangura@gmail.com>
2026-08-20 13:27:42 +0545

    high score add

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -38,16 +39,19 @@ function reducer(state, action) {
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
     case "finish":
-      return { ...state, status: "finished" };
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
     default:
       throw new Error("Action Unknown");
   }
 }
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initialState,
-  );
+  const [{ questions, status, index, answer, points, highscore }, dispatch] =
+    useReducer(reducer, initialState);
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce(
     (prev, cur) => prev + cur.points,
@@ -97,6 +101,7 @@ export default function App() {
             <FinishScreen
               points={points}
               maxPossiblePoints={maxPossiblePoints}
+              highscore={highscore}
             />
           )}
         </Main>
